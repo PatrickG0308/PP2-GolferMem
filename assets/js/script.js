cd9b923858e6fafe3f6f1efb1fa59ee68c3ae1b6
PatrickG0308 <>
2023-06-26 14:42:43 +0000

    added start game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,31 @@ const timeContainer = document.querySelector(".timer");
 // retrieve max number of matches required
 const MAX_MATCH = 8;
 const modal = document.getElementById("modal");
+
+// state of game
+let gameOn = false;
+
+// start of complete match scores
+let perfectMatch = 0;
+
+// manage flip state
+let flippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+//store number of moves (putts)
+let moves = 0;
+
+//store elapse time from timer
+let finalTime = "";
+
+// hide game board behind start page
+gameBoard.style.display = 'none';
+
+// listen for click of start button on start page
+// display game board 
+startBtn.addEventListener('click', () => {
+    startPage.style.display = 'none';
+    gameBoard.style.display = 'block';
+    startGame();
+});
