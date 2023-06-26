99848d2094a38306ebd99f780626d1bb0db0c654
PatrickG0308 <>
2023-06-26 14:30:34 +0000

    added const to script js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// retrieve start button from start page
+const startBtn = document.getElementById("start-btn");
+
+// retrieve start page element
+const startPage = document.getElementById("start-page");
+
+// retrieve gameboard element
+const gameBoard = document.getElementById("gameboard");
+
+// retrieve reset button element
+const resetBtn = document.getElementById("reset-btn");
+
+// retrieve memeory card element
+const cards = document.querySelectorAll('.memory-card');
+
+// retrieve moves(putts) 1st element
+const moveContainer = document.querySelector(".moves");
+
+// retrieve timer 1st element*/
+const timeContainer = document.querySelector(".timer");
+
+// retrieve max number of matches required
+const MAX_MATCH = 8;
+const modal = document.getElementById("modal");
